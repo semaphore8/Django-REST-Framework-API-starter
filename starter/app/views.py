17951bf03e34f343483b404c71b8ee4cda8fb269
sgbliznyuk <sgbliznyuk@gmail.com>
2020-03-11 09:44:18 +0300

    new initial state

--- starter/app/views.py
+++ starter/app/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from starter.app.serializers import UserSerializer, GroupSerializer
+from .models import Task
+from .serializers import TaskSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-
+ 
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-
+ 
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
